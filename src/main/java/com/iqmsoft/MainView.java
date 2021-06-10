2011fa0c3d5e70a31d7c8698186ed5d235ae02f0
Muruga <davanon2014@gmail.com>
2021-06-10 11:02:27 +0530

    First

---
+++ src/main/java/com/iqmsoft/MainView.java
@@ -0,0 +1,21 @@
+package com.iqmsoft;
+
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route
+@HtmlImport("css/shared-styles.html")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        add(button);
+    }
+}
